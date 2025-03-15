8fe70b727b331e84de4dfc3dd6c1467bea3266d9
Anton-rus <antonvlg91@gmail.com>
2025-03-15 15:28:23 +0000

    Add animation v1

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # app.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, CI/CD World!"
+    return render_template('index.html', message="Hello, CI/CD World!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
