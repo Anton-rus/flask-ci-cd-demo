f45b41c4da07f17091250c997a5aa0ded80cb869
Anton-rus <antonvlg91@gmail.com>
2025-03-15 15:52:32 +0000

    Add animation v3

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html', message="Hello, CI/CD World!")
+    return render_template('index.html', message="Lygushnis ka!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
